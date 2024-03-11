dae707356ed16216a45d8c474b1669b618416154
Supriya0029 <161030030+Supriya0029@users.noreply.github.com>
2024-03-11 12:35:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,273 @@
+ const song1 =new Audio ("kesariya .song1.mp3");
+ document.querySelector(".song1").addEventListener("click",function (){
+     song1.play();
+}) 
+document.querySelector(".song2").addEventListener("click",function (){
+     var song2 =new Audio ("kesariya .song1.mp3");
+     song1.pause();
+})
+const song3 =new Audio ("hanumanchalisa.mp3");
+document.querySelector(".song3").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song4").addEventListener("click",function (){
+     var song4 =new Audio ("hanumanchalisa.mp3");
+     song1.pause();
+})
+const song5 =new Audio ("baby clam down.mp3");
+document.querySelector(".song5").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song6").addEventListener("click",function (){
+     var song6 =new Audio ("baby clam down.mp3");
+     song1.pause();
+})
+const song7 =new Audio ("lag ja gale.mp3");
+document.querySelector(".song7").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song8").addEventListener("click",function (){
+     var song8 =new Audio ("lag ja gale.mp3");
+     song1.pause();
+})
+const song9 =new Audio ("pal pal.mp3");
+document.querySelector(".song9").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song10").addEventListener("click",function (){
+     var song10 =new Audio ("pal pal.mp3");
+     song1.pause();
+})
+const song11 =new Audio ("breakup song.mp3");
+document.querySelector(".song11").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song12").addEventListener("click",function (){
+     var song12 =new Audio ("breakup song.mp3");
+     song1.pause();
+})
+const song13 =new Audio ("dinero.mp3");
+document.querySelector(".song13").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song14").addEventListener("click",function (){
+     var song14 =new Audio ("dinero.mp3");
+     song1.pause();
+})
+const song15 =new Audio ("day light.mp3");
+document.querySelector(".song15").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song16").addEventListener("click",function (){
+     var song16 =new Audio ("day light.mp3");
+     song1.pause();
+})
+const song17 =new Audio ("Kyun(PaglaSongs).mp3");
+document.querySelector(".song17").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song18").addEventListener("click",function (){
+     var song18 =new Audio ("Kyun(PaglaSongs).mp3");
+     song1.pause();
+})
+const song19 =new Audio ("Door-Na-Raeen(PaglaSongs).mp3");
+document.querySelector(".song19").addEventListener("click",function (){
+     song1.play();
+})
+
+document.querySelector(".song20").addEventListener("click",function (){
+     var song20 =new Audio ("Door-Na-Raeen(PaglaSongs).mp3");
+     song1.pause();
+})
+const song21 =new Audio ("tere bin.mp3");
+document.querySelector(".song21").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song22").addEventListener("click",function (){
+     var song22 =new Audio ("tere bin.mp3");
+     song1.pause();
+})
+const song23 =new Audio ("Phir-Aur-Kya-Chahiye(PaglaSongs).mp3");
+document.querySelector(".song23").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song24").addEventListener("click",function (){
+     var song24 =new Audio ("Phir-Aur-Kya-Chahiye(PaglaSongs).mp3");
+     song1.pause();
+})
+const song25 =new Audio ("Man-Dhaga-Dhaga(PaglaSongs).mp3");
+document.querySelector(".song25").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song26").addEventListener("click",function (){
+     var song26 =new Audio ("Man-Dhaga-Dhaga(PaglaSongs).mp3");
+     song1.pause();
+})
+const song27 =new Audio ("natu natu.mp3");
+document.querySelector(".song27").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song28").addEventListener("click",function (){
+     var song28 =new Audio ("natu natu.mp3");
+     song1.pause();
+})
+const song29 =new Audio ("Ambarsariya(Pagal-World.Com.In).mp3");
+document.querySelector(".song29").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song30").addEventListener("click",function (){
+     var song30 =new Audio ("Ambarsariya(Pagal-World.Com.In).mp3");
+     song1.pause();
+})
+const song31 =new Audio ("The Humma Song (OK Jaanu)-(PagalWorld.Ink).mp3");
+document.querySelector(".song31").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song32").addEventListener("click",function (){
+     var song32 =new Audio ("The Humma Song (OK Jaanu)-(PagalWorld.Ink).mp3");
+     song1.pause();
+})
+const song33 =new Audio ("yo yo.mp3");
+document.querySelector(".song33").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song34").addEventListener("click",function (){
+     var song34 =new Audio ("yo yo.mp3");
+     song1.pause();
+})
+const song35 =new Audio ("Kudi-Nu-Nachne-De(PaglaSongs).mp3");
+document.querySelector(".song35").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song36").addEventListener("click",function (){
+     var song36 =new Audio ("Kudi-Nu-Nachne-De(PaglaSongs).mp3");
+     song1.pause();
+})
+const song37 =new Audio ("Char-Baj-Gaye(PaglaSongs) (1).mp3");
+document.querySelector(".song37").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song38").addEventListener("click",function (){
+     var song38 =new Audio ("Char-Baj-Gaye(PaglaSongs) (1).mp3");
+     song1.pause();
+})
+const song39 =new Audio ("Khalasi(PaglaSongs).mp3");
+document.querySelector(".song39").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song40").addEventListener("click",function (){
+     var song41 =new Audio ("Khalasi(PaglaSongs).mp3");
+     song1.pause();
+})
+const song41 =new Audio ("Duniyaa (Luka Chuppi)-(PagalWorld.Ink).mp3");
+document.querySelector(".song41").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song42").addEventListener("click",function (){
+     var song42 =new Audio ("Duniyaa (Luka Chuppi)-(PagalWorld.Ink).mp3");
+     song1.pause();
+})
+const song43 =new Audio ("Lutt-Putt-Gaya(PaglaSongs).mp3");
+document.querySelector(".song43").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song44").addEventListener("click",function (){
+     var song44 =new Audio ("Lutt-Putt-Gaya(PaglaSongs).mp3");
+     song1.pause();
+})
+const song45 =new Audio ("Drippy(PaglaSongs).mp3");
+document.querySelector(".song45").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song46").addEventListener("click",function (){
+     var song46 =new Audio ("Drippy(PaglaSongs).mp3");
+     song1.pause();
+})
+const song47 =new Audio ("Shape-of-You(PagalWorld).mp3");
+document.querySelector(".song47").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song48").addEventListener("click",function (){
+     var song48 =new Audio ("Shape-of-You(PagalWorld).mp3");
+     song1.pause();
+})
+const song49 =new Audio ("Dua-Lipa---Levitating(PagalWorld).mp3");
+document.querySelector(".song49").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song50").addEventListener("click",function (){
+     var song50 =new Audio ("Dua-Lipa---Levitating(PagalWorld).mp3");
+     song1.pause();
+})
+const song51 =new Audio ("Cheap-Thrills(PaglaSongs).mp3");
+document.querySelector(".song51").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song52").addEventListener("click",function (){
+     var song52 =new Audio ("Cheap-Thrills(PaglaSongs).mp3");
+     song1.pause();
+})
+const song53 =new Audio ("Umbrella(PaglaSongs).mp3");
+document.querySelector(".song53").addEventListener("click",function (){   
+     song1.play();
+})
+document.querySelector(".song54").addEventListener("click",function (){
+     var song54 =new Audio ("Umbrella(PaglaSongs).mp3");
+     song1.pause();
+})
+const song55 =new Audio ("Hass-Hass(PaglaSongs).mp3");
+document.querySelector(".song55").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song56").addEventListener("click",function (){
+     var song56=new Audio ("Hass-Hass(PaglaSongs).mp3");
+     song1.pause();
+})
+const song57 =new Audio ("Kal-Raat-Aaya-Mere-Ghar-Ek-Chor(PaglaSongs).mp3");
+document.querySelector(".song57").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song58").addEventListener("click",function (){
+     var song58 =new Audio ("Kal-Raat-Aaya-Mere-Ghar-Ek-Chor(PaglaSongs).mp3");
+     song1.pause();
+})
+const song59 =new Audio ("Attention(PaglaSongs).mp3");
+document.querySelector(".song59").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song60").addEventListener("click",function (){
+     var song60=new Audio ("Attention(PaglaSongs).mp3");
+     song1.pause();
+})
+const song61 =new Audio ("Saami(PaglaSongs).mp3");
+document.querySelector(".song61").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song62").addEventListener("click",function (){
+     var song62=new Audio ("Saami(PaglaSongs).mp3");
+     song1.pause();
+})
+const song63 =new Audio ("Butta-Bomma(PaglaSongs).mp3");
+document.querySelector(".song63").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song64").addEventListener("click",function (){
+     var song64=new Audio ("Butta-Bomma(PaglaSongs).mp3");
+     song1.pause();
+})
+const song65 =new Audio ("Sawarne-Lage(PaglaSongs).mp3");
+document.querySelector(".song65").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song66").addEventListener("click",function (){
+     var song66=new Audio ("Sawarne-Lage(PaglaSongs).mp3");
+     song1.pause();
+})
+const song67 =new Audio ("Oops(PaglaSongs).mp3");
+document.querySelector(".song67").addEventListener("click",function (){
+     song1.play();
+})
+document.querySelector(".song68").addEventListener("click",function (){
+     var song68=new Audio ("Oops(PaglaSongs).mp3");
+     song1.pause();
+})
